6033cdad85de8e9d95a8da41a2aaa0d9b22ad84c
Hana Kim <hsk003@ucsd.edu>
2021-05-16 13:57:19 -0700

    changed register path

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ let count = 0;
 // Make sure you register your service worker here too
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/Lab7/sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
